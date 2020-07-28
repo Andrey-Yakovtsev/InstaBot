c1b92fd0a9e5e89a8aad50bd44f909c7c4508865
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-07-28 08:59:55 +0300

    Перенес отписки внутрь рабочих функций, чтобы был разрыв между логинами по 12 часов

--- InstaPy2_0.py
+++ InstaPy2_0.py
@@ -34,7 +34,7 @@ def like_by_tags_bot():
         enabled=True,
         like=3, comment=5,
         follow=4,
-        unfollow=3,
+        unfollow=10,
         randomize=True,
         random_range_from=70,
         random_range_to=140)
@@ -49,6 +49,13 @@ def like_by_tags_bot():
 
     session.set_skip_users(skip_private=True, skip_no_profile_pic=True,
                            skip_bio_keyword=['доставка', 'заказ', 'оплата', 'магазин'])  # skip_business=True,
+    session.unfollow_users(amount=15,
+                           instapy_followed_enabled=True,
+                           instapy_followed_param="nonfollowers",
+                           style="FIFO",
+                           unfollow_after=90 * 60 * 60)
+
+    session.set_dont_unfollow_active_users(enabled=True, posts=3)
 
     session.set_quota_supervisor(enabled=True, peak_comments_daily=240,
                                  peak_comments_hourly=21,
@@ -91,9 +98,9 @@ def follow_user_followers_bot():
     session.set_mandatory_language(enabled=True, character_set=['CYRILLIC'])
     session.set_action_delays(
         enabled=True,
-        like=3, comment=5,
-        follow=4,
-        unfollow=3,
+        like=4, comment=5,
+        follow=5,
+        unfollow=7,
         randomize=True,
         random_range_from=70,
         random_range_to=140)
@@ -103,6 +110,14 @@ def follow_user_followers_bot():
     # session.set_delimit_commenting(enabled=True, max_comments=None, min_comments=1)
 
     session.set_comments(['Классно!', 'Здорово!'])
+
+    session.unfollow_users(amount=15,
+                           instapy_followed_enabled=True,
+                           instapy_followed_param="nonfollowers",
+                           style="FIFO",
+                           unfollow_after=90 * 60 * 60)
+
+    session.set_dont_unfollow_active_users(enabled=True, posts=3)
 
     session.set_relationship_bounds(enabled=True, max_followers=1020500, min_posts=3)
 
@@ -115,16 +130,6 @@ def follow_user_followers_bot():
                                  peak_follows_hourly=20,
                                  sleep_after=["likes", "comments_d", "follows", "unfollows"],
                                  notify_me=True)
-
-
-
-    # зафолловить лайкеров фоточек именованных аккаунтов
-    # session.follow_likers(to_follow_list,
-    #                       photos_grab_amount=3,
-    #                       follow_likers_per_photo=3,
-    #                       randomize=True,
-    #                       sleep_delay=600,
-    #                       interact=False)
 
     telegram.end()
     session.end()
@@ -158,15 +163,15 @@ def follow_photo_likers_bot():
                           photos_grab_amount=3,
                           follow_likers_per_photo=3,
                           randomize=True,
-                          sleep_delay=600,
-                          interact=False)
+                          sleep_delay=4,
+                          interact=True)
     session.set_dont_include(skipped_friends)
     session.set_mandatory_language(enabled=True, character_set=['CYRILLIC'])
     session.set_action_delays(
         enabled=True,
-        like=3, comment=5,
-        follow=4,
-        unfollow=3,
+        like=4, comment=5,
+        follow=5,
+        unfollow=8,
         randomize=True,
         random_range_from=70,
         random_range_to=140)
@@ -189,51 +194,48 @@ def follow_photo_likers_bot():
                                  sleep_after=["likes", "comments_d", "follows", "unfollows"],
                                  notify_me=True)
 
-    # список аккаунтов, по подписчикам которых наддо профолловить
-    # session.follow_user_followers(to_follow_list, amount=5, randomize=False, sleep_delay=300)
-
-
-
     telegram.end()
     session.end()
     end = datetime.now()
     print('Время окончания:', end)
     print('Время работы:', end-start)
 
-def my_unsubscriber_bot():
-    '''Задание для отписок'''
-    # logging.basicConfig(filename="logs/unsubscribe.log", level=logging.INFO)
-    session = InstaPy(
-        username="trisport_russia",
-        password="Pivovar3312",
-        headless_browser=True,
-        disable_image_load=True,
-        multi_logs=True,
-        bypass_security_challenge_using='sms',
-        show_logs=True
-    )
-    telegram = InstaPyTelegramBot(token='1095292391:AAHpAyz2zfnkQmHzq53rJ8ce_2BfpHa09LI',
-                                  telegram_username='@andrey_yakovtsev',
-                                  debug=True,
-                                  instapy_session=session)
-    session.login()
-    start = datetime.now()
-    print('Время начала:', start)
 
-    session.set_action_delays(unfollow=60)
-
-    session.unfollow_users(amount=20,
-                           instapy_followed_enabled=True,
-                           instapy_followed_param="nonfollowers",
-                           style="FIFO",
-                           unfollow_after=90*60*60)
-
-    session.set_dont_unfollow_active_users(enabled=True, posts=3)
-    telegram.end()
-    session.end()
-    end = datetime.now()
-    print('Время завершения:', end)
-    print('Время работы:', end - start)
+'''Перенес отписки в стандариный функционал'''
+# def my_unsubscriber_bot():
+#     '''Задание для отписок'''
+#     # logging.basicConfig(filename="logs/unsubscribe.log", level=logging.INFO)
+#     session = InstaPy(
+#         username="trisport_russia",
+#         password="Pivovar3312",
+#         headless_browser=True,
+#         disable_image_load=True,
+#         multi_logs=True,
+#         bypass_security_challenge_using='sms',
+#         show_logs=True
+#     )
+#     telegram = InstaPyTelegramBot(token='1095292391:AAHpAyz2zfnkQmHzq53rJ8ce_2BfpHa09LI',
+#                                   telegram_username='@andrey_yakovtsev',
+#                                   debug=True,
+#                                   instapy_session=session)
+#     session.login()
+#     start = datetime.now()
+#     print('Время начала:', start)
+#
+#     session.set_action_delays(unfollow=60)
+#
+#     session.unfollow_users(amount=20,
+#                            instapy_followed_enabled=True,
+#                            instapy_followed_param="nonfollowers",
+#                            style="FIFO",
+#                            unfollow_after=90*60*60)
+#
+#     session.set_dont_unfollow_active_users(enabled=True, posts=3)
+#     telegram.end()
+#     session.end()
+#     end = datetime.now()
+#     print('Время завершения:', end)
+#     print('Время работы:', end - start)
 
 schedule.every().monday.at("17:45").do(follow_photo_likers_bot)
 schedule.every().wednesday.at("17:45").do(follow_photo_likers_bot)
@@ -246,8 +248,8 @@ schedule.every().saturday.at("17:45").do(follow_user_followers_bot)
 
 
 schedule.every().day.at("06:23").do(like_by_tags_bot)
-schedule.every().day.at("14:04").do(my_unsubscriber_bot)
-schedule.every().day.at("01:54").do(my_unsubscriber_bot)
+# schedule.every().day.at("14:04").do(my_unsubscriber_bot)
+# schedule.every().day.at("01:54").do(my_unsubscriber_bot)
 
 
 
