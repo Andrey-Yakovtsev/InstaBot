5a85cb388ecb02f34101f5d092f9af4b03acdf8c
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-08-14 12:53:14 +0300

    Headless try

--- instarobov3.py
+++ instarobov3.py
@@ -20,9 +20,9 @@ class InstagramBot():
 
         self.username = username
         self.password = password
-        self.browser = webdriver.Firefox()
-        # self.options = Options()
-        # self.options.headless = True
+        self.options = Options()
+        self.options.headless = True
+        self.browser = webdriver.Firefox(options=self.options)
 
 
     # метод для закрытия браузера
